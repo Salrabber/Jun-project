7ba1eacb32d56d80f2ed7a198a071672f59acb32
Salrabber <salrabber@gmail.com>
2023-04-01 18:32:41 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,7 @@
+$(function () {
+  $(".todolist__slider").slick({
+    dots: false,
+    prevArrow: '<button type="button" class="slick-prev">Previous</button>',
+    nextArrow: '<button type="button" class="slick-next">Next</button>',
+  });
+});
